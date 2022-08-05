b65485685a8200333b13792e10252910ffced5cd
Vy Nguyen <vy1872@gmail.com>
2022-08-04 22:08:46 -0700

    inquirer for the engineer is now set up

--- index.js
+++ index.js
@@ -33,4 +33,62 @@ const getManager = () => {
     ])
 }
 
-getManager();
+const getEngineer = () => {
+    inquirer.prompt([
+        {
+            type: `input`,
+            name: `name`,
+            message: `What is the engineer's name?`,
+            validate(answer) {return ((!/^[a-zA-Z\s]+$/.test(answer)) ? `Names contain letters` : true)}
+        },
+        {
+            type: `input`,
+            name: `id`,
+            message: `What is the engineer's id?`,
+            validate(answer) {return ((!/^[0-9]+$/.test(answer)) ? `Ids should only contain numbers` : true)}
+        },
+        {
+            type: `input`,
+            name: `email`,
+            message: `What is the engineer's email?`,
+            validate(answer) {return ((!/^\S+@\S+\.\S+$/.test(answer)) ? `email format example: blah@blah.com` : true)}
+        },
+        {
+            type: `input`,
+            name: `github`,
+            message: `What is the engineer's github username?`,
+            validate(answer) {return ((!/^[A-Za-z0-9\-]*$/.test(answer)) ? `The github username should only letters, numbers, or hyphens` : true)}
+        },
+    ])
+}
+
+const getIntern = () => {
+    inquirer.prompt([
+        {
+            type: `input`,
+            name: `name`,
+            message: `What is the intern's name?`,
+            validate(answer) {return ((!/^[a-zA-Z\s]+$/.test(answer)) ? `Names contain letters` : true)}
+        },
+        {
+            type: `input`,
+            name: `id`,
+            message: `What is the intern's id?`,
+            validate(answer) {return ((!/^[0-9]+$/.test(answer)) ? `Ids should only contain numbers` : true)}
+        },
+        {
+            type: `input`,
+            name: `email`,
+            message: `What is the intern's email?`,
+            validate(answer) {return ((!/^\S+@\S+\.\S+$/.test(answer)) ? `email format example: blah@blah.com` : true)}
+        },
+        {
+            type: `input`,
+            name: `school`,
+            message: `Where did the intern graduate from?`,
+            validate(answer) {return ((!/^[a-zA-Z\s]+$/.test(answer)) ? `The school's name should only contain letters` : true)}
+        },
+    ])
+}
+
+getEngineer();
