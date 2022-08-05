b2e7305ced9e6fcc20e83db9a5470ab5fb9b0a1e
Vy Nguyen <vy1872@gmail.com>
2022-08-04 21:56:38 -0700

    got the inquirer to work and made the inquirer for manager

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-const Employee = require("./lib/Employee");
+const inquirer = require(`inquirer`);
+const fs = require(`fs`);
+const Manager = require(`./lib/Manager`);
+const Engineer = require(`./lib/Engineer`);
+const Intern = require(`./lib/Intern`);
 
-const employee = new Employee();
+const getManager = () => {
+    inquirer.prompt([
+        {
+            type: `input`,
+            name: `name`,
+            message: `What is the manager's name?`,
+            validate(answer) {return ((!/^[a-zA-Z\s]+$/.test(answer)) ? `Names contain letters` : true)}
+        },
+        {
+            type: `input`,
+            name: `id`,
+            message: `What is the manager's id?`,
+            validate(answer) {return ((!/^[0-9]+$/.test(answer)) ? `Ids should only contain numbers` : true)}
+        },
+        {
+            type: `input`,
+            name: `email`,
+            message: `What is the manager's email?`,
+            validate(answer) {return ((!/^\S+@\S+\.\S+$/.test(answer)) ? `email format example: blah@blah.com` : true)}
+        },
+        {
+            type: `input`,
+            name: `officeNumber`,
+            message: `What is the manager's office number?`,
+            validate(answer) {return ((!/^[0-9]+$/.test(answer)) ? `The office number should only contain numbers` : true)}
+        },
+    ])
+}
 
-employee.create();
+getManager();
